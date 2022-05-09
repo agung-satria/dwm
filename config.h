8bbd837b03b3dc38de26bbd983b5558472f17589
Agung Satria <agungsatria890@gmail.com>
2022-05-09 22:01:52 +0700

    add user bh

--- config.h
+++ config.h
@@ -16,6 +16,8 @@ static const unsigned int gappov    = 12;       /* vert outer gap between window
 static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+
 /*  Display modes of the tab bar: never shown, always shown, shown only in  */
 /*  monocle mode in the presence of several windows.                        */
 /*  Modes after showtab_nmodes are disabled.                                */
@@ -41,8 +43,8 @@ static const char *colors[][3]    = {
 
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
-static const char *tags[] = { "", "🌏", "🎨", "🕹", "🐧", "💻", "📰", "📂", "🔍" };
-/* static const char *tags[] = { "", "", "", "", "", "", "", "", "" }; */
+/* static const char *tags[] = { "", "🌏", "🎨", "🕹", "🐧", "💻", "📰", "📂", "🔍" }; */
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 /* xprop(1):
